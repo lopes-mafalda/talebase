6d16ae73b575b48941bc0f0b0c257af333a488ac
lopes-mafalda <lopes.mafalda97@gmail.com>
2025-06-13 17:54:31 +0100

    Initial commit: created base project structure with Spring Initializr

---
+++ src/main/java/dev/mafaldalopes/talebase/TaleBaseApplication.java
@@ -0,0 +1,13 @@
+package dev.mafaldalopes.talebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaleBaseApplication.class, args);
+	}
+
+}
